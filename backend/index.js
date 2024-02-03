5be3eb0f020f521122464ff71d232ced067c1de0
zuxiangw <zuxiangwang2002@gmail.com>
2024-02-03 16:42:40 -0500

    temp main menu page

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,19 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const { User, Game, PlayerGameStats, Message } = require('./models');
+const { User, Game, PlayerGameStats, Message } = require("./models");
 
-const fs = require('fs').promises; 
-const filePath = './wordList.txt';
+const fs = require("fs").promises;
+const filePath = "./wordList.txt";
 
-mongoose.connect('mongodb://127.0.0.1:27017/guess5', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected.'))
-  .catch(err => {
-    console.error('Database connection error:', err);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/guess5", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected."))
+  .catch((err) => {
+    console.error("Database connection error:", err);
     process.exit(1);
   });
 
@@ -89,7 +93,6 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
 // Route to get stats
 app.get("/stats", async (req, res) => {
   try {
@@ -102,24 +105,24 @@ app.get("/stats", async (req, res) => {
 
 app.get("/stats/lasthour", async (req, res) => {
   try {
-    const oneHourAgo = new Date(new Date().getTime() - (60*60*1000));
+    const oneHourAgo = new Date(new Date().getTime() - 60 * 60 * 1000);
 
     const lastHourGames = await Game.aggregate([
       {
         $match: {
-          timeStarted: { $gte: oneHourAgo }
-        }
+          timeStarted: { $gte: oneHourAgo },
+        },
       },
       {
         $lookup: {
           from: "playergamestats",
           localField: "_id",
           foreignField: "game_id",
-          as: "playerStats"
-        }
+          as: "playerStats",
+        },
       },
       {
-        $unwind: "$playerStats"
+        $unwind: "$playerStats",
       },
       {
         $group: {
@@ -127,19 +130,19 @@ app.get("/stats/lasthour", async (req, res) => {
           totalGuesses: { $sum: "$playerStats.totalGuesses" },
           secondsPlayed: { $sum: "$playerStats.secondsPlayed" },
           gamesPlayed: { $sum: 1 },
-          gamesWon: { $sum: { $cond: ["$playerStats.isWinner", 1, 0] } }
-        }
+          gamesWon: { $sum: { $cond: ["$playerStats.isWinner", 1, 0] } },
+        },
       },
       {
         $lookup: {
           from: "users",
           localField: "_id",
           foreignField: "_id",
-          as: "playerInfo"
-        }
+          as: "playerInfo",
+        },
       },
       {
-        $unwind: "$playerInfo"
+        $unwind: "$playerInfo",
       },
       {
         $project: {
@@ -148,9 +151,9 @@ app.get("/stats/lasthour", async (req, res) => {
           totalGuesses: 1,
           secondsPlayed: 1,
           gamesPlayed: 1,
-          gamesWon: 1
-        }
-      }
+          gamesWon: 1,
+        },
+      },
     ]);
 
     res.json(lastHourGames);
@@ -160,20 +163,17 @@ app.get("/stats/lasthour", async (req, res) => {
   }
 });
 
-
-
-
 // Socket.io for real-time updates
-io.on('connection', (socket) => {
-  console.log('New client connected');
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
-  socket.on('gameCompleted', async () => {
+  socket.on("gameCompleted", async () => {
     const stats = await User.find({});
-    io.sockets.emit('updateStats', stats);
+    io.sockets.emit("updateStats", stats);
   });
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
@@ -183,12 +183,11 @@ app.post("/user", async (req, res) => {
     const user = new User(req.body);
     await user.save();
     res.status(201).send(user);
-    io.sockets.emit('updateStats'); // Emit an event to update stats (This is just for testing purposes, we should remove it later).
+    io.sockets.emit("updateStats"); // Emit an event to update stats (This is just for testing purposes, we should remove it later).
   } catch (err) {
     res.status(400).send(err);
   }
-}
-);
+});
 
 // Create a game and playerstats (This is just for testing purposes, we should remove it later).
 app.post("/game", async (req, res) => {
@@ -204,12 +203,12 @@ app.post("/game", async (req, res) => {
       game_id: game._id,
       totalGuesses: req.body.totalGuesses,
       secondsPlayed: req.body.secondsPlayed,
-      isWinner: req.body.isWinner
+      isWinner: req.body.isWinner,
     });
     await playerGameStats.save();
 
     res.status(201).send(playerGameStats);
-    io.sockets.emit('updateStats'); // Emit an event to update stats (This is just for testing purposes, we should remove it later).
+    io.sockets.emit("updateStats"); // Emit an event to update stats (This is just for testing purposes, we should remove it later).
   } catch (err) {
     res.status(400).send(err);
   }
@@ -218,11 +217,11 @@ app.post("/game", async (req, res) => {
 // Function to check if a word exists in a .txt file
 async function checkWordExists(word) {
   try {
-    const data = await fs.readFile(filePath, 'utf8');
+    const data = await fs.readFile(filePath, "utf8");
     const words = data.split(/\r?\n/); // Split the file content by new line to get an array of words
     return words.includes(word); // Check if the word exists in the array
   } catch (err) {
-    console.error('Error reading file:', err);
+    console.error("Error reading file:", err);
     return false; // Return false in case of an error
   }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,39 @@ import { SocketContextProvider } from "./providers/SocketProvider";
 import GuessingController from "./GuessingController";
 import CurrentGuesses from "./CurrentGuesses";
 
+import StatScreen from "./statScreen";
+
+import { useState } from "react";
+
 function App() {
+  const [page, setPage] = useState("main");
+
   return (
     <div className="App">
       <SocketContextProvider>
-        <GameRoomContextProvider>
-          <GuessingController />
-          <CurrentGuesses />
-        </GameRoomContextProvider>
+        {page === "main" && <MainPage setPage={setPage} />}
+        {page === "game" && <GamePage />}
       </SocketContextProvider>
     </div>
   );
 }
+
+const MainPage = ({ setPage }) => {
+  return (
+    <div>
+      <button onClick={() => setPage("game")}>Game screen</button>
+      <StatScreen />
+    </div>
+  );
+};
+
+const GamePage = () => {
+  return (
+    <GameRoomContextProvider>
+      <GuessingController />
+      <CurrentGuesses />
+    </GameRoomContextProvider>
+  );
+};
 
 export default App;
