6c8c4d6378ba12b281b4afd1c6b0315f65321ab0
zuxiangw <zuxiangwang2002@gmail.com>
2024-02-05 21:13:59 -0500

    user input word list update

--- frontend/src/userLists.js
+++ frontend/src/userLists.js
@@ -1,23 +1,142 @@
-import React from 'react';
-import './userLists.css';
+import React from "react";
+import "./userLists.css";
+import { useState } from "react";
 
 const ListBoxContainer = () => {
+  const [confirmedIn, setConfirmedIn] = useState([]);
+  const [candidates, setCandidates] = useState([]);
+  const [confirmedOut, setConfirmedOut] = useState([]);
+  const [inputValueConfirmedIn, setInputValueConfirmedIn] = useState("");
+  const [inputValueCandidates, setInputValueCandidates] = useState("");
+  const [inputValueConfirmedOut, setInputValueConfirmedOut] = useState("");
+
+  const handleAdd = (value, list, setList, setInputValue) => {
+    if (value.trim() === "") return;
+
+    if (list.includes(value)) {
+      window.alert("Alphabet already inserted!");
+    } else {
+      setList((prevList) => [...prevList, value]);
+      setInputValue("");
+    }
+  };
+
+  const handleDelete = (index, list, setList) => {
+    const newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  };
+
   return (
-    <div className="list-box-container">
-      <div>
-        <label className="list-label">Confirmed In</label>
-        <textarea className="text-area"></textarea>
+    <div className="alphabet-list-container">
+      <div className="input-container">
+        <label>Confirmed In:</label>
+        <input
+          type="text"
+          value={inputValueConfirmedIn}
+          onChange={(e) =>
+            setInputValueConfirmedIn(e.target.value.toUpperCase())
+          }
+        />
+        <button
+          onClick={() =>
+            handleAdd(
+              inputValueConfirmedIn,
+              confirmedIn,
+              setConfirmedIn,
+              setInputValueConfirmedIn
+            )
+          }
+        >
+          Add
+        </button>
+        <div className="alphabet-list">
+          {confirmedIn.map((char, index) => (
+            <div key={index}>
+              {char}{" "}
+              <button
+                onClick={() => handleDelete(index, confirmedIn, setConfirmedIn)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <label className="list-label">Confirmed Out</label>
-        <textarea className="list-text-area"></textarea>
+
+      <div className="input-container">
+        <label>Candidates:</label>
+        <input
+          type="text"
+          value={inputValueCandidates}
+          onChange={(e) =>
+            setInputValueCandidates(e.target.value.toUpperCase())
+          }
+        />
+        <button
+          onClick={() =>
+            handleAdd(
+              inputValueCandidates,
+              candidates,
+              setCandidates,
+              setInputValueCandidates
+            )
+          }
+        >
+          Add
+        </button>
+        <div className="alphabet-list">
+          {candidates.map((char, index) => (
+            <div key={index}>
+              {char}{" "}
+              <button
+                onClick={() => handleDelete(index, candidates, setCandidates)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <label className="list-label">Candidates</label>
-        <textarea className="list-text-area"></textarea>
+
+      <div className="input-container">
+        <label>Confirmed Out:</label>
+        <input
+          type="text"
+          value={inputValueConfirmedOut}
+          onChange={(e) =>
+            setInputValueConfirmedOut(e.target.value.toUpperCase())
+          }
+        />
+        <button
+          onClick={() =>
+            handleAdd(
+              inputValueConfirmedOut,
+              confirmedOut,
+              setConfirmedOut,
+              setInputValueConfirmedOut
+            )
+          }
+        >
+          Add
+        </button>
+        <div className="alphabet-list">
+          {confirmedOut.map((char, index) => (
+            <div key={index}>
+              {char}{" "}
+              <button
+                onClick={() =>
+                  handleDelete(index, confirmedOut, setConfirmedOut)
+                }
+              >
+                X
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default ListBoxContainer;
