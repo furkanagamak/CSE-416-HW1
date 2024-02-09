edc89e603215f8129989b3a9fb6689a0ee97b1ee
Lee Vincent <chanju.lee@stonybrook.edu>
2024-02-09 12:02:47 -0500

    added countdown

--- frontend/src/HomeScreen.js
+++ frontend/src/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen = ({ setPage }) => {
   return (
     <div className='HomeScreen'>
       <h1 className='HomeTitle'>GUESS5</h1>
-      <h2 className='Welcome'>Welcome back, </h2>
+      <h2 className='Welcome'>Welcome </h2>
       <button className="FindGame" onClick={() => setPage("game")}>Find Game</button>
       <button className='ViewStats' onClick={handleViewStats}>View Stats</button>
     </div>
