3dc471cb692514157cc009ba63daf0749b2bf855
zuxiangw <zuxiangwang2002@gmail.com>
2024-02-09 20:19:28 -0500

    adding game rule to home screen

--- frontend/src/HomeScreen.js
+++ frontend/src/HomeScreen.js
@@ -11,11 +11,56 @@ const HomeScreen = ({ setPage }) => {
   };
 
   return (
-    <div className='HomeScreen'>
-      <h1 className='HomeTitle'>GUESS5</h1>
-      <h2 className='Welcome'>Welcome, {username}</h2>
-      <button className="FindGame" onClick={() => setPage("game")}>Find Game</button>
-      <button className='ViewStats' onClick={handleViewStats}>View Stats</button>
+    <div className="HomeScreen">
+      <h1 className="HomeTitle">GUESS5</h1>
+      <h2 className="Welcome">Welcome, {username}</h2>
+      <button className="FindGame" onClick={() => setPage("game")}>
+        Find Game
+      </button>
+      <button className="ViewStats" onClick={handleViewStats}>
+        View Stats
+      </button>
+      <div className="game-rules">
+        <h2>Game Rules:</h2>
+        <ul className="game-rules-list">
+          <li className="game-rules-main">
+            There are two players, player A and player B.
+          </li>
+          <li className="game-rules-main">
+            Both players choose a 5 letter secret word with no repeating
+            letters.
+          </li>
+          <li className="game-rules-main">
+            Once both players have submitted a valid word, one player is
+            randomly selected to go first.
+          </li>
+          <li className="game-rules-main">
+            Each player takes turn being the guessing player:
+          </li>
+          <li className="game-rules-main">
+            The guessing player has 60 seconds to submit a valid 5 letter guess
+            word.
+          </li>
+          <li className="game-rules-sub">
+            If the 60 seconds passes and no submission has been made, the other
+            player becomes the guessing player.
+          </li>
+          <li className="game-rules-sub">
+            Guesses restricted to 5 letter real words, can have repeating
+            letters.
+          </li>
+          <li className="game-rules-sub">
+            If the guess word is the secret word of the other player, then the
+            guessing player won.
+          </li>
+          <li className="game-rules-sub">
+            If the guess word is not the secret word of the other player, then
+            the system should notify the guessing player how many letters of the
+            guess word are within the other player’s secret word. Then the other
+            player should take turns being the guessing player.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
