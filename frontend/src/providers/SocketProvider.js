0590e405d561cec5c64981c0eb96bccd6bcf2950
furkanagamak <furkanagamak06@gmail.com>
2024-02-09 23:21:42 -0500

    Fixed the chat button and some eslint warnings.

--- frontend/src/providers/SocketProvider.js
+++ frontend/src/providers/SocketProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext } from "react";
 import io from "socket.io-client";
 import { useUserContext } from "./UserProvider";
 
